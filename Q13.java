6916710eb4b92d66d839fd33bff953d4d39b69f7
sanjayjoker7 <sanjaisp7@gmail.com>
2025-09-25 23:40:43 +0530

    Removed duplicate Java files outside JAVA_BASICS

--- Q13.java
+++
@@ -1,8 +0,0 @@
-package JAVA_BASICS;
-//Print "Hello, World!" using System.out.println.
-public class Q13 {
-    
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
-    }
-}
--- Q16.java
+++
@@ -1,9 +0,0 @@
-package JAVA_BASICS;
-//Print a formatted string using System.out.printf (e.g., %.2f).
-public class Q16 {
-    public static void main(String[] args){
-        double value = 123.456789;
-        System.out.printf("Formatted value: " +value);
-    }
-    
-}
